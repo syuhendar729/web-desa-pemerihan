6adb92db72afbaf068f51eea0c52fcb871cac19b
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-12-30 21:12:22 +0700

    coding and making ui and input logic in admin/dashboard/shop

---
+++ app/admin/dashboard/shop/addItem/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { useState } from "react";
+import { IoSend } from "react-icons/io5";
+
+export default function Page() {
+  const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleUpload = () => {
+
+  }
+
+  console.log(title)
+
+  return (
+    <>
+      <div className="m-10">
+        {/* Input nama barang */}
+        <div className="flex items-center gap-5 mb-5">
+          <p>Nama Barang:</p>
+          <input
+            className="border px-2 py-1 border-gray-300 w-1/2"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        {/* Input harga barang */}
+        <div className="flex items-center gap-5 mb-5">
+          <p>Harga:</p>
+          <input
+            className="border px-2 py-1 border-gray-300 w-1/2"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+
+        {/* Input gambar */}
+        <div className="flex items-center gap-5 mb-5">
+          <p>Gambar utama:</p>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => {
+              setFile(e.target.files?.[0] || null);
+            }}
+            className="flex-1 block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+          />
+        </div>
+        <div className="flex items-center gap-5 mb-5">
+          <p>(Opsional) Gambar Tambahan 1</p>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => {
+              setFile(e.target.files?.[0] || null);
+            }}
+            className="flex-1 block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+          />
+        </div>
+        <div className="flex items-center gap-5 mb-5">
+          <p>(Opsional) Gambar Tambahan 2</p>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => {
+              setFile(e.target.files?.[0] || null);
+            }}
+            className="flex-1 block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+          />
+        </div>
+
+        {/* Input deskripsi */}
+        <div className="flex gap-5 mb-5 flex-col md:flex-row">
+          <p>Masukan deskripsi barang:</p>
+          <textarea
+            className="border px-2 py-1 border-gray-300 w-full md:w-1/2"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+
+
+        {/* Tombol kirim */}
+        <div className="my-5 flex justify-end">
+          <div
+            className="rounded-2xl text-sm px-4 py-2 bg-blue-50 text-blue-700 font-bold cursor-pointer hover:bg-blue-100"
+            onClick={handleUpload}
+          >
+            <div className="flex items-center gap-2">
+              <p>Kirim Artikel</p>
+              <IoSend />
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
+}
--- app/admin/dashboard/shop/page.tsx
+++ app/admin/dashboard/shop/page.tsx
@@ -1,3 +1,79 @@
+"use client";
+import Link from "next/link";
+import DashboardSidebar from "@/ui/dashboardSidebar";
+import { MdAddBusiness } from "react-icons/md";
+import { useEffect, useState } from "react";
+
 export default function Page() {
-  return <>belum dibuat</>;
+  const [articles, setArticles] = useState<any>([]);
+
+  const getArticleData = async () => {
+    try {
+      const res = await fetch(
+        "http://localhost:3000/api/article?page=1&limit=10",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      if (!res.ok) {
+        throw new Error("Request failed");
+      }
+
+      const data = await res.json();
+      console.log(data);
+      setArticles(data.data);
+
+      localStorage.setItem("auth", data.token);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getArticleData();
+  }, []);
+
+  return (
+    <div className="flex flex-col md:flex-row min-h-dvh bg-white">
+      <DashboardSidebar />
+
+      <main className="flex-1 p-5 md:p-8 overflow-x-hidden">
+        <div className="font-bold text-xl mb-6">Toko</div>
+
+        <div className="mb-6 flex">
+          <Link prefetch={false} href="/admin/dashboard/shop/addItem">
+            <span className="flex items-center gap-2 rounded-2xl py-2 px-4 bg-blue-50 text-blue-700 font-bold cursor-pointer hover:bg-blue-100 text-sm transition-colors">
+              <MdAddBusiness className="text-xl" />
+              Tambah Barang di Toko
+            </span>
+          </Link>
+        </div>
+
+        {articles.map((article: any) => (
+          <div key={article.id} className="flex flex-col gap-4 mb-5">
+            <div className="border rounded-lg p-4 flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 shadow-sm">
+              <div>
+                <p className="text-gray-700 truncate max-w-md font-bold">
+                  {article.title}
+                </p>
+              </div>
+
+              <div className="flex gap-3 text-sm font-medium">
+                <button className="px-3 py-1 text-blue-600 hover:bg-blue-50 rounded">
+                  Edit
+                </button>
+                <button className="px-3 py-1 text-red-600 hover:bg-red-50 rounded">
+                  Hapus
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
 }
